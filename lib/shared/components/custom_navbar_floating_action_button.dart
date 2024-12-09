a4a0fd55d6762277c1e4f1b0c24a6ac0bb8947a2
Alben Bustamante <albenbustamante@gmail.com>
2024-12-08 20:59:17 -0500

    add: simple dialog into floating action button

--- lib/shared/components/custom_navbar_floating_action_button.dart
+++ lib/shared/components/custom_navbar_floating_action_button.dart
@@ -12,11 +12,31 @@ class CustomNavbarFloatingActionButton extends StatelessWidget {
         BoxShadow(color: Colors.white, blurRadius: 1.0, spreadRadius: 7.0)
       ]),
       child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            showDialog(context: context, builder: (context) => _dialog());
+          },
           backgroundColor: theme.colorScheme.primary,
           foregroundColor: Colors.white,
           shape: const CircleBorder(),
           child: const Icon(Icons.add_outlined, size: 40.0)),
     );
+  }
+
+  SimpleDialog _dialog() {
+    return SimpleDialog(
+        title: Text('¿Qué quieres agregar?', style: theme.textTheme.titleLarge),
+        children: [
+          _option('Nuevo gasto', () {}),
+          const SizedBox(height: 5.0),
+          _option('Nuevo ingreso', () {})
+        ]);
+  }
+
+  SimpleDialogOption _option(String text, void Function() onTap) {
+    return SimpleDialogOption(
+        onPressed: () => onTap(),
+        padding: const EdgeInsets.symmetric(horizontal: 36.0, vertical: 12.0),
+        child: Text(text,
+            style: TextStyle(fontSize: 20.0, color: Colors.grey[700])));
   }
 }
